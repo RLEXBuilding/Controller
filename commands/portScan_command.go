87dab876dc586c9d3749f45f80e97e3cb1888b88
mdway <romangraef@gmail.com>
2017-12-19 17:15:07 +0100

    Added features to portscan

--- commands/portScan_command.go
+++ commands/portScan_command.go
@@ -21,32 +21,53 @@ func (command PortScanCommand) String() string {
 }
 
 func (command PortScanCommand) Execute(kill chan bool, args []string) {
-	if len(args) < 3 {
-		fmt.Println("portscan <address> <port-from> <port-to>")
+	if len(args) < 1 {
+		fmt.Println("portscan <address> [port-from-inclusive] [port-to-exclusive] [timeout-in-milliseconds]")
+		return
 	}
 
 	address := args[0]
-	from, err := strconv.Atoi(args[1])
-	if err != nil {
-		fmt.Println("The second argument is not a valid number")
-		return
+	from := 0
+	to := 65535
+	var timeout = 2000 * time.Millisecond
+	if len(args) >= 3 {
+		var err error
+		from, err = strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Println("The second argument is not a valid number")
+			return
+		}
+		to, err = strconv.Atoi(args[2])
+		if err != nil {
+			fmt.Println("The third argument is not a valid number")
+			return
+		}
 	}
-	to, err := strconv.Atoi(args[2])
-	if err != nil {
-		fmt.Println("The third argument is not a valid number")
-		return
+	if len(args) >= 4 {
+		iTimeout, err := strconv.Atoi(args[3])
+		if err != nil || to <= 0 {
+			fmt.Println("Invalid timeout.")
+			return
+		}
+		timeout = time.Duration(iTimeout) * time.Millisecond
 	}
 
-	ps := portscanner.NewPortScanner(address, 2*time.Second, 5)
+	ps := portscanner.NewPortScanner(address, timeout, 5)
 
 	// get opened port
 	fmt.Printf("scanning port %d-%d...\n", from, to)
 
-	openedPorts := ps.GetOpenedPort(from, to)
+	for port := from; port < to; port++ {
+		select {
+		case <-kill:
+			fmt.Println("Aborted.")
+			return
 
-	for i := 0; i < len(openedPorts); i++ {
-		port := openedPorts[i]
-		fmt.Print(" ", port, " [open]")
-		fmt.Println("  -  ", ps.DescribePort(port))
+		default:
+			if ps.IsOpen(port) {
+				fmt.Print(" ", port, " [open]")
+				fmt.Println("  -  ", ps.DescribePort(port))
+			}
+		}
 	}
 }
